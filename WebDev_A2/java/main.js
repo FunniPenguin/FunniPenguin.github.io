c747e04ddda5d8994182f0818db261da750ef2a9
FunniPenguin <yapdylan845@gmail.com>
2025-08-04 00:01:32 +0800

    Add files via upload

--- WebDev_A2/java/main.js
+++ WebDev_A2/java/main.js
@@ -22,6 +22,7 @@ const qns2 = document.getElementById("q2");
 const qns2display = document.getElementById("q2display");
 const qns3 = document.getElementById("q3");
 const qns3display = document.getElementById("q3display");
+const respg = document.querySelector(".result");
 
 const prevbtn = document.querySelector("#prev");
 const nextbtn = document.querySelector("#next");
@@ -31,6 +32,7 @@ var allquestions = document.querySelectorAll(".question");
 //ints and bools
 var muteAudio = false;
 var currqn = 1;
+var score = 0;
 
 const menuItemsList = document.querySelector("ul");
 const hamIcon = document.querySelector("#hamIcon");
@@ -40,7 +42,7 @@ function toggleMenus() { /*open and close menu*/
     //if menuItemsList dont have the class "menuShow", add it, else remove it
     menuItemsList.classList.toggle("show");
     //if menu is showing (has the class “menuShow”)
-    if (menuItemsList.classList.contains("menuShow")) {
+    if (menuItemsList.classList.contains("show")) {
         hamIcon.innerHTML = "Close Menu"; //change button text to chose menu
 
     } else { //if menu NOT showing
@@ -66,15 +68,68 @@ function toggleAudio() {
 }
 
 function hideqns() {
-    for (let qns of allquestions) { //go through all subtopic pages
+    for (let qns of allquestions) { //go through all question tabs
         qns.style.display = "none"; //hide it
     }
 }
 function initForm() {
+    prevbtn.style.display = "block";
+    nextbtn.style.display = "block";
+    nextbtn.innerHTML = "Next"
+    respg.style.display = "none";
     hideqns();
     let question1 = document.querySelector("#question1");
     question1.style.display = "block";
     currqn = 1;
+    // Did not reset the value of questions because checked and reset() are not working for me
+    // However, you still can retake the survey without changing the page, just press the page button in the nav
+    // It will allow you to edit your response and resubmit
+}
+function submitForm() {
+    var answer;
+    score = 0;
+    for (i = 1; i <= 5; i++) {
+
+        if (parseInt(i) == 2) {
+            answer = qns2.value;
+            score -= parseInt(answer) - 3;
+        }
+        else if (parseInt(i) == 3) {
+            // Minus 3 because neutral is 3 so the value should be negative
+            answer = qns3.value;
+            score += parseInt(answer) - 3;
+        }
+        else {
+            try { answer = parseInt(document.querySelector("input[name='q" + i + "']:checked").value); }
+            catch {
+                alert("Please answer all the questions before submitting.");
+                return;
+            }
+            score += parseInt(answer);
+        }
+    }
+    // Since no errors, proceed to display results page
+    //begin with removing the questions
+    hideqns();
+    //hide the prev and next btns
+    prevbtn.style.display = "none";
+    nextbtn.style.display = "none";
+    // display page
+    respg.style.display = "flex";
+    var scoreDisp = document.querySelector(".score-display");
+    // enter score
+    scoreDisp.innerHTML = score;
+    //if overall score is positive, capitalist. else if score is negative, commie. else neutral.
+    var restext = document.querySelector(".result-text");
+    if (parseInt(score) > 0) {
+        restext.innerHTML = "Congratulations, you are a capitalist!";
+    }
+    else if (parseInt(score) < 0) {
+        restext.innerHTML = "Congratulations, you are a communist!";
+    }
+    else {
+        restext.innerHTML = "Congratulations, you are a neither!";
+    }
 }
 function run() {
     //insert game loop into here
@@ -149,19 +204,22 @@ learn6.addEventListener("click", function () {
 });
 
 //Qns java
-qns2.oninput = function() {
+qns2.oninput = function () {
     //this is the this pointer of qns2 range html, value refers to the number the slider is on
-  qns2display.innerHTML = `Current number: ${this.value}`
+    qns2display.innerHTML = `Current number: ${this.value}`;
 }
-qns3.oninput = function() {
+qns3.oninput = function () {
     //this is the this pointer of qns2 range html, value refers to the number the slider is on
-  qns3display.innerHTML = `Current number: ${this.value}`
+    qns3display.innerHTML = `Current number: ${this.value}`;
 }
 
 // Prev and next button functions
 prevbtn.addEventListener("click", function () {
     clickAudio.play();
     if (currqn > 1) {
+        if (currqn == 5) {
+            nextbtn.innerHTML = "Next";
+        }
         currqn--;
         hideqns();
         let question = document.querySelector("#question" + currqn);
@@ -170,15 +228,18 @@ prevbtn.addEventListener("click", function () {
 })
 nextbtn.addEventListener("click", function () {
     clickAudio.play();
-    if (currqn < 3) { //Number in if statement is max number of questions
+    if (currqn < 5) { //Number in if statement is max number of questions
         currqn++;
         hideqns();
         let question = document.querySelector("#question" + currqn);
         question.style.display = "block";
+        if (currqn == 5) {
+            this.innerHTML = "Submit";
+        }
     }
-    // else{
-    //     //submit form
-    // }
+    else if (currqn == 5) {
+        submitForm();
+    }
 })
 
 //Run the init functions here to create the start page
